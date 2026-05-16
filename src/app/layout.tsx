881a0151da7b9f879b9e0c879db6aa2df32a6839
Leroi Kakatsi <kingiscoding@gmail.com>
2026-05-16 12:43:14 +0100

    refactor: remove BrandEmblem component (water animation on auth pages) and update layout styling for cleaner BrandPanel interface

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,6 +63,7 @@ export default function RootLayout({
     <html lang="fr" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         <GoogleAnalytics />
         <PageSense />
